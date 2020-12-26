d1f40aaecc5de60de0f10f7ebae5283a6428c808
Mr. Orleond <dev.mr.orleond@gmail.com>
2020-12-26 08:02:20 +0300

    Adding a new Variable

--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
 "use strict";
 
-const money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+const money = 1000,
+    income = 'Халтура',
+    addExpenses = 'Подарки, продукты',
+    deposit = false,
+    mission = 5000,
+    period = 6,
+    budgetDay = Math.floor(money / 30);
+
+    console.log('money: ', typeof money);
+    console.log('income: ', typeof income);
+    console.log('deposit: ', typeof deposit);
+    console.log('addExpenses: ', addExpenses.length);    
+    console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей.`);    
+    console.log('addExpenses: ', addExpenses.toLowerCase().split(', '));
+    console.log('budgetDay: ', budgetDay);
